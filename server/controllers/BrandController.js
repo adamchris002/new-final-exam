1a55bf74955a50ea33ffc45fc273bb7d3a48ca10
Adam Christoper <adamchris002@gmail.com>
2022-09-05 14:02:02 +0700

    new-final-exam

--- server/controllers/BrandController.js
+++ server/controllers/BrandController.js
@@ -1,8 +1,73 @@
-const {brand} = require("../models");
+const { brand } = require("../models");
 
 class BrandController {
+  static async getBrand(req, res) {
+    try {
+      let result = await brand.findAll();
+      res.json(result);
+    } catch (err) {
+      res.json(err);
+    }
+  }
+  static async addPage(req, res) {}
+  static async add(req, res) {
+    try {
+      const { name, image, city, total_employees } = req.body;
+      let result = await brand.create({
+        name,
+        image,
+        city,
+        total_employees,
+      });
+      res.json(result);
+    } catch (err) {
+      res.json(err);
+    }
+  }
+  static async updatePage(req, res) {}
+  static async update(req, res) {
+    try {
+      const id = Number(req.params.id);
 
+      const { name, image, city, total_employees } = req.body;
+
+      let result = await brand.update(
+        {
+          name,
+          image,
+          city,
+          total_employees,
+        },
+        {
+          where: { id },
+        }
+      );
+
+      result[0] === 1
+        ? res.json({
+            message: `Brand with id of ${id} has been updated`,
+          })
+        : res.json({
+            message: `Brand with id of ${id} has not been updated`,
+          });
+    } catch (err) {
+      res.json(err);
+    }
+  }
+  static async delete(req, res) {
+    const id = Number(req.params.id);
+
+    let result = await brand.destroy({
+      where: { id },
+    });
+    result === 1
+      ? res.json({
+          message: `Brand with id of ${id} has been deleted`,
+        })
+      : res.json({
+          message: `Brand with id of ${id} has not been deleted`,
+        });
+  }
 }
 
 module.exports = BrandController;
-
--- server/controllers/FruitController.js
+++ server/controllers/FruitController.js
@@ -1,7 +1,77 @@
-const {fruit} = require('../models');
+const { fruit } = require("../models");
 
 class FruitController {
+  static async getFruit(req, res) {
+    try {
+      let result = await fruit.findAll();
+      res.json(result);
+    } catch (err) {
+      res.json(err);
+    }
+  }
+  static async addPage(req, res) {}
+  static async add(req, res) {
+    try {
+      const { name, image, price, stock, brandId, categoryId } = req.body;
+      let result = await fruit.create({
+        name,
+        image,
+        price,
+        stock,
+        brandId,
+        categoryId,
+      });
+      res.json(result);
+    } catch (err) {
+      res.json(err);
+    }
+  }
+  static async updatePage(req, res) {}
+  static async update(req, res) {
+    try {
+      const id = Number(req.params.id);
 
+      const { name, image, price, stock, brandId, categoryId } = req.body;
+
+      let result = await fruit.update(
+        {
+          name,
+          image,
+          price,
+          stock,
+          brandId,
+          categoryId,
+        },
+        {
+          where: { id },
+        }
+      );
+
+      result[0] === 1
+        ? res.json({
+            message: `Fruit with id of ${id} has been updated`,
+          })
+        : res.json({
+            message: `Fruit with id of ${id} has not been updated`,
+          });
+    } catch (err) {
+      res.json(err);
+    }
+  }
+  static async delete(req, res) {
+    const id = Number(req.params.id);
+
+    let result = await fruit.destroy({
+      where: { id },
+    });
+    result === 1
+      ? res.json({
+          message: `Fruit with id of ${id} has been deleted`,
+        })
+      : res.json({
+          message: `Fruit with id of ${id} has not been deleted`,
+        });
+  }
 }
 
 module.exports = FruitController;
